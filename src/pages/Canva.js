f1949c5f5853b22e93c1216ca74553459b829b39
Samuel Vieira <vieira.samuel@outlook.com.br>
2023-05-13 20:58:22 -0300

    exibe nos do grafo no canva

---
+++ src/pages/Canva.js
@@ -0,0 +1,82 @@
+import cytoscape from "cytoscape";
+
+import { useEffect } from "react";
+
+export default function Visualization({ elements, setNodeName }) {
+    function renderCytoscape() {
+        console.log(elements);
+        const cy = cytoscape({
+            container: document.getElementById("cy"), // container to render in
+        
+            //elements: [
+            // list of graph elements to start with
+            elements: elements,
+            //{
+            // edge ab
+            //  data: { id: "ab", source: "a", target: "b" },
+            // },
+            // {
+            //   data: { id: "aa", source: "a", target: "a" },
+            // },
+            // {
+            //   data: { id: "ac", source: "a", target: "c" },
+            // },
+            //],
+        
+            style: [
+              // the stylesheet for the graph
+              {
+                selector: "node",
+                style: {
+                  "background-color": "#666",
+                  label: "data(id)",
+                },
+              },
+        
+              {
+                selector: "edge",
+                style: {
+                  width: 3,
+                  "line-color": "#ccc",
+                  "target-arrow-color": "#ccc",
+                  "target-arrow-shape": "triangle",
+                  "curve-style": "bezier",
+                },
+              },
+            ],
+
+            layout: {
+              name: "grid",
+              rows: 10,
+              
+            },
+            zoom: 1,
+            minZoom: 1,
+            maxZoom: 10,
+            nodeSpacing: 5,
+            animate: true,
+            randomize: true,
+          });
+        
+          cy.on("click", "node", (evt) => {
+            let node = evt.target;
+            console.log("tapped " + node.id());
+            handleClick(node);
+          });
+
+          //cy.nodes().sort().layout({name: "circle",})
+    }
+
+    const handleClick = (node) => {
+        setNodeName(node.id());
+      };
+
+    useEffect(() => {
+        console.log("cytoscapeCanvas", elements);
+        renderCytoscape()
+      }, [elements]);
+
+  
+  
+  return <main id="cy" className="h-screen col-start-1 col-span-2"></main>;
+}
--- src/pages/Visualization.js
+++ src/pages/Visualization.js
@@ -1,101 +1,44 @@
-import { useEffect, useState } from "react";
-import cytoscape from "cytoscape";
+import { useCallback, useEffect, useState } from "react";
+import axios from "axios";
 
 import Header from "./Header";
 import SideBar from "./SideBar";
-
+import Canva from "./Canva";
 
 export default function Visualization() {
-  const [nodeName, setNodeName] = useState('');
+  const [nodeName, setNodeName] = useState("");
+  const [isElementsLoaded, setIsElementsLoaded] = useState(false);
 
   let cy;
 
-function cytoscapeCanva() {
+  //async function cytoscapeCanva() {
+  function getElements() {
+    axios
+      .get("http://localhost:3000/api/grn")
+      .then((response) => {
+        //console.log(response.data);
+        setElements(response.data);
+        setIsElementsLoaded(true);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  const [elements, setElements] = useState([]);
+
+
+  //}
+
   useEffect(() => {
-    cy = cytoscape({
-      container: document.getElementById('cy'), // container to render in
-    
-      elements: [
-        // list of graph elements to start with
-        {
-          // node a
-          data: { id: "a" },
-        },
-        {
-          // node b
-          data: { id: "b" },
-        },
-        {
-          data: { id: "c" },
-        },
-        {
-          // edge ab
-          data: { id: "ab", source: "a", target: "b" },
-        },
-        {
-          data: { id: "aa", source: "a", target: "a" },
-        },
-        {
-          data: { id: "ac", source: "a", target: "c" },
-        },
-      ],
-    
-      style: [
-        // the stylesheet for the graph
-        {
-          selector: "node",
-          style: {
-            "background-color": "#666",
-            label: "data(id)",
-          },
-        },
-    
-        {
-          selector: "edge",
-          style: {
-            width: 3,
-            "line-color": "#ccc",
-            "target-arrow-color": "#ccc",
-            "target-arrow-shape": "triangle",
-            "curve-style": "bezier",
-          },
-        },
-      ],
-    
-      layout: {
-        name: "grid",
-        rows: 1,
-      },
-      zoom: 2,
-      minZoom: 1,
-      maxZoom: 5,
-    });
-
-    cy.on('click', 'node', (evt) => {
-      let node = evt.target;
-      console.log('tapped ' + node.id());
-      handleClick(node);
-    });
-
+    getElements();
   }, []);
-}
-
-const handleClick = (node) => {
-  setNodeName(node.id());
-  console.log(nodeName);
-  sidebar(nodeName);
-}
-
-function sidebar(nodeName) {
-  return <SideBar nodeName={nodeName}/>
-}
 
   return (
     <div className="w-screen grid grid-cols-3">
       <Header />
-      <main id="cy" className="h-screen col-start-1 col-span-2"></main>
-      {cytoscapeCanva()}
-      {sidebar(nodeName)}
+      {isElementsLoaded ? <Canva elements={ elements } setNodeName={setNodeName}/> : <h1>"Carregando..."</h1>}
+      <SideBar nodeName={nodeName} />
     </div>
   );
 }
