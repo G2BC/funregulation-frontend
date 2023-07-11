bc6f7614da9631390eba2760343f0decec2f25bf
Samuel Vieira <vieira.samuel@outlook.com.br>
2023-07-10 23:14:21 -0300

    add visualizacao de organismo selecionado

--- src/pages/Visualization.js
+++ src/pages/Visualization.js
@@ -1,4 +1,5 @@
 import { useCallback, useEffect, useState } from "react";
+import { useRouter } from 'next/router';
 import axios from "axios";
 
 import Header from "./Header";
@@ -9,12 +10,15 @@ export default function Visualization() {
   const [nodeName, setNodeName] = useState("");
   const [isElementsLoaded, setIsElementsLoaded] = useState(false);
 
+  const router = useRouter();
+  const { organism } = router.query;
+
   let cy;
 
   //async function cytoscapeCanva() {
   function getElements() {
     axios
-      .get("http://localhost:3000/api/grn")
+      .get("http://localhost:3000/api/grn", { params: {organism: organism} })
       .then((response) => {
         //console.log(response.data);
         setElements(response.data);
@@ -37,7 +41,8 @@ export default function Visualization() {
   return (
     <div className="w-screen grid grid-cols-3">
       <Header />
-      {isElementsLoaded ? <Canva elements={ elements } setNodeName={setNodeName}/> : <h1>"Carregando..."</h1>}
+      {console.log(organism)}
+      {isElementsLoaded ? <Canva elements={ elements } setNodeName={setNodeName}/> : <h1 className="h-screen col-start-1 col-span-2">"Carregando..."</h1>}
       <SideBar nodeName={nodeName} />
     </div>
   );
--- src/pages/api/grn.js
+++ src/pages/api/grn.js
@@ -4,18 +4,26 @@ import connection from "@/lib/db"
 
 const conn = connection;
 
+export const config = {
+  api: {
+    responseLimit: false,
+  },
+}
+
 export default async function handler(req, res) {
   try {
-    const query = 'SELECT DISTINCT ON (tf_name) tf_name FROM network.relations;'
+    const {organism} = req.query;
+    //const query = 'SELECT DISTINCT ON (tf_name) tf_name FROM network.relations;'
+    const query = `SELECT DISTINCT * from public.model_regulatory model right join public.gene gen on model.tf_locus_tag = gen.locus_tag AND gen.organism = '${organism}' WHERE model.tf_locus_tag IS NOT NULL;`
     const result = await conn.query(query);
     const { rows } = result;
     const initialData = JSON.stringify(rows);
     const finalData = JSON.parse(initialData);
-    finalData.forEach((dados) => {
-      let data = {id: dados['tf_name']}
-      dados['data'] = data;
-      delete dados['tf_name'];
-    });
+    // finalData.forEach((dados) => {
+    //   let data = {id: dados['tf_name']}
+    //   dados['data'] = data;
+    //   delete dados['tf_name'];
+    // });
     return res.status(200).json(finalData)
   } catch ( error ) {
     console.log(error);
