5be84f9da1e6eeb82560a01639dee8b121a2ccf7
Samuel Vieira <vieira.samuel@outlook.com.br>
2023-07-12 10:22:52 -0300

    add funcao handleSelectChange para atualizar parametro de organismo selecionado

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Header from "./Header";
+import Link from 'next/link';
 
 export default function Search() {
     const [genes, setGenes] = useState([])
     const [isElementsLoaded, setIsElementsLoaded] = useState(false);
+    const [organism, setOrganism] = useState('');
 
     function getGenes() {
         axios
@@ -21,12 +23,17 @@ export default function Search() {
       useEffect(() => {
         getGenes();
       }, []);
+
+      function handleSelectChange(e) {
+        setOrganism(e.target.value);
+      }
+
     return(
         <div className="w-screen grid grid-cols-3">
             <Header/>
             <main className="w-screen h-96 flex justify-center items-center col-start-1 col-end-3">
-                <select className="w-96 h-8">
-                    {/*<option default value="">Selecione umasopção</option>*/}
+                <select className="w-96 h-8" onChange={handleSelectChange}>
+                    <option default value="">Selecione uma opção</option>
                     {isElementsLoaded ? genes.map(gene => 
                       <option key={gene.organism} value={gene.organism}>{gene.organism}</option>
                     ) : <option key={1}value="">Selecione uma opção</option>}
@@ -35,7 +42,8 @@ export default function Search() {
                     <option value="GRN2">Gene Regulatory Network 2</option>
                     <option value="GRN3">Gene Regulatory Network 3</option> */}
                 </select>
-                <button className="w-24 h-8 bg-azul-700 text-branco">Pesquisar</button>
+                <Link href={{pathname: '/Visualization', query:{ organism: organism }}}><button className="w-24 h-8 bg-azul-700 text-branco">Pesquisar</button></Link>
+                
             </main>
         </div>
     );
