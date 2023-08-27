0969d30c12eb2f1f61685dc9acd2f2b6b792c303
Samuel Vieira <vieira.samuel@outlook.com.br>
2023-08-26 22:22:47 -0300

    altera requisicao para arquivo json exemplo

--- src/pages/api/grn.js
+++ src/pages/api/grn.js
@@ -1,6 +1,8 @@
 import connection from "@/lib/db"
 import axios from "axios";
 
+import data from '../../lib/data.json';
+
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 const conn = connection;
@@ -21,13 +23,13 @@ export default async function handler(req, res) {
     //const initialData = JSON.stringify(rows);
     //const finalData = JSON.parse(initialData);
 
-    axios
-        .get("http://localhost:5500/src/lib/data.json")
-        .then((response) => {
-          //console.log(response.data);
-          return res.status(200).json(response.data)
-        })
-        
+    // axios
+    //     .get("http://localhost:5500/src/lib/data.json")
+    //     .then((response) => {
+    //       //console.log(response.data);
+    //       return res.status(200).json(response.data)
+    //     })
+    return res.status(200).json(data)
     // finalData.forEach((dados) => {
     //   let data = {id: dados['tf_name']}
     //   dados['data'] = data;
