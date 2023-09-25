555fec647e38f04160375d9b450342b4f2bfa74e
Samuel Vieira <vieira.samuel@outlook.com.br>
2023-09-25 00:49:13 -0300

    add filtro por tags

--- src/pages/SideBar.js
+++ src/pages/SideBar.js
@@ -1,4 +1,6 @@
-export default function SideBar({ nodeName, circleLayout, saveGraphState, loadGraphState, savedElements, exportGraph, filterElements, restoreGraph }) {
+import Select from 'react-select';
+
+export default function SideBar({ nodeName, circleLayout, saveGraphState, loadGraphState, savedElements, exportGraph, filterElements, restoreGraph, elements, filter, setFilter, tfs}) {
   return (
     <aside className="col-start-3 bg-azul-500 text-branco">
       <h1 className="m-4 text-center text-xl font-semibold">Detalhes</h1>
@@ -12,8 +14,15 @@ export default function SideBar({ nodeName, circleLayout, saveGraphState, loadGr
       <button type="button" onClick={exportGraph}>Export Graph</button>
       <br />
       <br />
-      <input id="search" type="text"></input>
-      <button type="button" onClick={() => filterElements(document.getElementById('search').value)}>Filter</button>
+      <Select
+        isMulti
+        name="tfs"
+        options={tfs}
+        className="basic-multi-select text-preto"
+        classNamePrefix="select"
+        onChange={(choices) => setFilter(choices)}
+      />
+      <button type="button" onClick={() => filterElements(filter)}>Filter</button>
       <br />
       <button type="button" onClick={restoreGraph}>Restore</button>
       <div id="png-eg" className="w-10 h-10"></div>
