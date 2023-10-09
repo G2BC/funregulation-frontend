2efe17a099f4fa0ac709bcf4b1495a0bd76960ea
Samuel Vieira <vieira.samuel@outlook.com.br>
2023-10-09 10:44:24 -0300

    add secao de informacoes do no selecionado

--- src/pages/SideBar.js
+++ src/pages/SideBar.js
@@ -3,7 +3,7 @@ import Select from 'react-select';
 export default function SideBar({ nodeName, circleLayout, saveGraphState, loadGraphState, savedElements, exportGraph, filterElements, restoreGraph, elements, filter, setFilter, tfs}) {
   return (
     <aside id="sidebar" className="col-start-3 bg-azul-500 text-branco">
-      <h1 className="m-4 text-center text-xl font-semibold">Detalhes</h1>
+      <h1 className="m-4 text-center text-xl font-semibold">Estatísticas da Rede</h1>
       <h5 className = "font-semibold">Nome proteina: {nodeName}</h5>
       <br />
       <button type="button" onClick={saveGraphState}>Save State</button>
@@ -13,6 +13,7 @@ export default function SideBar({ nodeName, circleLayout, saveGraphState, loadGr
       <button type="button" onClick={exportGraph}>Export Graph</button>
       <br />
       <div id="png-eg" className="w-10 h-10"></div>
+      <h1 className="m-4 text-center text-xl font-semibold">Informações do nó</h1>
     </aside>
   );
 }
