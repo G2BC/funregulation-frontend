72e724be151b74fa3d92addd5495f6647b9a0993
Samuel Vieira <vieira.samuel@outlook.com.br>
2023-12-03 22:57:16 -0300

    add exibicao de anotacoes funcionais do no selecionado

--- src/pages/SideBar.js
+++ src/pages/SideBar.js
@@ -1,7 +1,24 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from "axios";
 
-export default function SideBar({ nodeName}) {
+export default function SideBar({ nodeName }) {
   const [isInfoLoaded, setIsInfoLoaded] = useState(true);
+  const [infoProtein, setInfoProtein] = useState({});
+
+  function getNodeInfo() {
+    axios
+      .get("http://localhost:3000/api/node_info", { params: { node_id: nodeName } })
+      .then((response) => {
+        setInfoProtein(response.data);
+      }
+      ).catch((error) => {
+        console.error(error);
+      });
+  }
+
+  useEffect(() => {
+    getNodeInfo();
+  }, [nodeName]);
 
   return (
     <aside id="sidebar" className="col-start-3 bg-azul-500 text-branco">
@@ -20,20 +37,37 @@ export default function SideBar({ nodeName}) {
       {isInfoLoaded ? (
         <div className="w-full m-2 p-1">
           <h3 className = "font-semibold">Node name: {nodeName}</h3>
-          <h3>Degree Centrality</h3>
+          {/* <h3>Degree Centrality</h3>
           <h3>Closeness Centrality</h3>
           <h3>Betweenness Centrality</h3>
           <h3>Eigenvector Centrality</h3>
-          <h3>Harmonic Centrality</h3>
-          <h3>Interpro</h3>
-          <h3>EC</h3>
-          <h3>Cazy</h3>
-          <h3>PFAN</h3>
-          <h3>Panther</h3>
-          <h3>Genome3d</h3>
-          <h3>Uniprot</h3>
-          <h3>GO</h3>
-          <h3>Reactome</h3>
+          <h3>Harmonic Centrality</h3> */}
+          <h3 className = "font-semibold inline">Interpro: </h3>
+          <a target="_blank" href={`https://www.ebi.ac.uk/interpro/entry/InterPro/${infoProtein.interpro}/`} className='inline'>{infoProtein.interpro}</a>
+          <br/>
+          <h3 className = "font-semibold inline">EC: </h3>
+          <a target="_blank" href={`https://www.ebi.ac.uk/intenz/query?cmd=SearchEC&ec=${infoProtein.ec_number}`} className='inline'>{infoProtein.ec_number}</a>
+          <br/>
+          <h3 className = "font-semibold inline">Cazy: </h3>
+          <a target="_blank" href={`http://www.cazy.org/${infoProtein.cazy}.html`} className='inline'>{infoProtein.cazy}</a>
+          <br/>
+          <h3 className = "font-semibold inline">PFAN: </h3>
+          <a target="_blank" href={`https://www.ebi.ac.uk/interpro/entry/pfam/${infoProtein.pfam}/`} className='inline'>{infoProtein.pfam}</a>
+          <br/>
+          <h3 className = "font-semibold inline">Panther: </h3>
+          <a target="_blank" href={`https://www.ebi.ac.uk/interpro/entry/panther/${infoProtein.panther}/`} className='inline'>{infoProtein.panther}</a>
+          <br/>
+          <h3 className = "font-semibold inline">Genome3d: </h3>
+          <a target="_blank" href={`http://www.genome3d.net/uniprot/id/${infoProtein.gene3d}/annotations`} className='inline'>{infoProtein.gene3d}</a>
+          <br/>
+          <h3 className = "font-semibold inline">Uniprot: </h3>
+          <a target="_blank" href={`https://www.uniprot.org/uniprotkb/${infoProtein.uniprot}`} className='inline'>{infoProtein.uniprot}</a>
+          <br/>
+          <h3 className = "font-semibold inline">GO: </h3>
+          <a target="_blank" href={`https://www.ebi.ac.uk/QuickGO/term/GO:${infoProtein.go}`} className='inline'>{infoProtein.go}</a>
+          <br/>
+          <h3 className = "font-semibold inline">Reactome: </h3>
+          <a target="_blank" href={`https://reactome.org/content/detail/${infoProtein.reactome}`} className='inline'>{infoProtein.reactome}</a>
         </div>
       ) : (<h3 className="text-center">Select a node to see details</h3>)}
     </aside>
